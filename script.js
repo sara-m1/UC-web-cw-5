ea85c49c63478bd7ad92a482a89e5a6697c7f45b
sara-m1 <142673557+sara-m1@users.noreply.github.com>
2023-09-04 19:40:43 +0300

    done

---
+++ script.js
@@ -0,0 +1,27 @@
+alert("مرحبا بك في موقعي انا سارا وسوف احسب درجتك")
+let grade =parseInt(prompt("ادخل درجتك"))
+console.log(grade)
+console.grade("black")
+if(grade>=90 && grade <= 100){
+    console.log("لقد حصلت على امتياز 🥳")
+}
+else if(grade>=80 && grade<=89){
+    console.log("لقد حصلت على جيد جداً🤩")
+}
+else if(grade>=70 && grade<=79){
+    console.log("لقد حصلت على جيد🙂")
+}
+else if(grade>=60 && grade<=69){
+    console.log("قد حصلت على مقبول😕")
+}
+else if(grade>=50 && grade<=59){
+    console.log("لقد حصلت على ضعيف☹️")
+}
+else if(grade<50){
+    console.log("راسب💔")
+}
+// جافاسكربت ليست جافا
+
+
+
+
